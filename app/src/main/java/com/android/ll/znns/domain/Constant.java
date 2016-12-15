20b9c513d0a85e59bf4d6deed4e59a2e9330ec13
822 <2851729134@qq.com>
2016-12-15 15:13:54 +0800

    1.1.0

--- app/src/main/java/com/android/ll/znns/domain/Constant.java
+++ app/src/main/java/com/android/ll/znns/domain/Constant.java
@@ -3,11 +3,11 @@ package com.android.ll.znns.domain;
 public class Constant {
     public static final String BASE_URL = "http://sj.zol.com.cn/bizhi";
     public static final String BASE_HOST = "http://sj.zol.com.cn";
-    public static final String NEW = "/page/";
-    public static final String XINGGAN = "/cat/xinggan/page/";
-    public static final String SHAONV = "/cat/shaonv/page/";
-    public static final String MRXT = "/cat/mrxt/page/";
-    public static final String SWMT = "/cat/swmt/page/";
-    public static final String WMXZ = "/cat/wmxz/page/";
+    public static final String NEW = "/dongman/";
+    public static final String XINGGAN = "/katong/";
+    public static final String SHAONV = "/dongman/";
+    public static final String MRXT = "/mingxing/";
+    public static final String SWMT = "/dongman/";
+    public static final String WMXZ = "/chuangyi/";
     public static final String WALLPAPER = "/youxi/";
 }
