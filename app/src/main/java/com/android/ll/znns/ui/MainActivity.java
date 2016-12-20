fd88c567df1d4d8dcf992b31cdfeeda004c69c90
822 <2851729134@qq.com>
2016-12-20 12:53:12 +0800

    加入qq群

--- app/src/main/java/com/android/ll/znns/ui/MainActivity.java
+++ app/src/main/java/com/android/ll/znns/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.android.ll.znns.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -9,16 +11,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.ll.znns.R;
 import com.android.ll.znns.ui.mainImageList.widget.fragment.DMFragment;
-import com.android.ll.znns.ui.mainImageList.widget.fragment.MRXTFragment;
-import com.android.ll.znns.ui.mainImageList.widget.fragment.QTFragment;
-import com.android.ll.znns.ui.mainImageList.widget.fragment.SWMTFragment;
-import com.android.ll.znns.ui.mainImageList.widget.fragment.ShaoNvFragment;
 import com.android.ll.znns.ui.mainImageList.widget.fragment.GamesFragment;
-import com.android.ll.znns.ui.mainImageList.widget.fragment.WMXZFragment;
+import com.android.ll.znns.ui.mainImageList.widget.fragment.QTFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -99,13 +98,16 @@ public class MainActivity extends AppCompatActivity
                         .content(R.string.about_app)
                         .show();
                 break;
-            case R.id.nav_personal:
-                new MaterialDialog.Builder(MainActivity.this)
-                        .title("关于作者")
-                        .icon(getResources().getDrawable(R.drawable.ic_personal))
-                        .positiveText("确定")
-                        .content(R.string.about_account)
-                        .show();
+//            case R.id.nav_personal:
+//                new MaterialDialog.Builder(MainActivity.this)
+//                        .title("关于作者")
+//                        .icon(getResources().getDrawable(R.drawable.ic_personal))
+//                        .positiveText("确定")
+//                        .content(R.string.about_account)
+//                        .show();
+//                break;
+            case R.id.nav_join_us:
+                joinQQGroup("WP2QK9cpRqOc9WxNETv5lUC5axNYbQgy");
                 break;
         }
         mToolbar.setTitle(item.getTitle());
@@ -113,6 +115,30 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /****************
+     *
+     * 发起添加群流程。群号：二次元(464192221) 的 key 为： WP2QK9cpRqOc9WxNETv5lUC5axNYbQgy
+     * 调用 joinQQGroup(WP2QK9cpRqOc9WxNETv5lUC5axNYbQgy) 即可发起手Q客户端申请加群 二次元(464192221)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     ******************/
+    public boolean joinQQGroup(String key) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            Toast.makeText(MainActivity.this, "未安装手Q或安装的版本不支持" ,Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+
 
 
     // change fragment
